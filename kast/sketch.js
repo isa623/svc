de2c571a9a357b999194c62ce6a03d376256c2b2
isa623 <NEXT16325@edu.nextkbh.dk>
2025-02-22 12:50:25 +0100

    Pogram med knapper som kan starte og pause

--- kast/sketch.js
+++ kast/sketch.js
@@ -1,27 +1,73 @@
 let balls = [];
 let angleInputs = [];
 let speedInputs = [];
-
+let running = false;
+let oldt = 0;
 
 function setup() {
 	createCanvas(800, 800);
 	angleMode(DEGREES);
+
 	for (let i = 0; i < 3; i++) {
-		let angle = 45;
-		let speed = 500;
-		balls.push(new Bold(50, angle, speed));
+		let angle = (i + 1) * 15
+		let angleInput = createInput(angle.toString());
+		angleInput.position(10, height + 10 + i * 30);
+		angleInputs.push(angleInput);
+
+		let v0 = 300 + (i + 1) * 100
+		let speedInput = createInput(v0.toString());
+		speedInput.position(70, height + 10 + i * 30);
+		speedInputs.push(speedInput);
 	}
+
+	let startButton = createButton("Start");
+	startButton.position(10, height + 100);
+	startButton.mousePressed(start);
+
+	let pauseButton = createButton("Pause");
+	pauseButton.position(70, height + 100);
+	pauseButton.mousePressed(pause);
+
+	let videreButton = createButton("Videre");
+	videreButton.position(140, height + 100);
+	videreButton.mousePressed(videre);
 }
 
 function draw() {
-	t = millis() / 1000;
-	background('blue');
-	for (let ball of balls) {
-		ball.opdater(t);
-		ball.show();
-
+	if (running) {
+		t = millis() / 1000;
+		deltat = t - oldt;
+		background('blue');
+		for (let ball of balls) {
+			ball.opdater(deltat);
+			ball.show();
+		}
+		oldt = t;
+	} else {
+		oldt = millis() / 1000;
 	}
 }
+
+function start() {
+	running = true;
+	balls = [];
+	for (let i = 0; i < 3; i++) {
+		let angle = parseFloat(angleInputs[i].value());
+		let speed = parseFloat(speedInputs[i].value());
+		if (!isNaN(angle) && !isNaN(speed)) {
+			balls.push(new Bold(50, angle, speed));
+		}
+	}
+}
+
+function pause() {
+	running = false;
+}
+
+function videre() {
+	running = true;
+}
+
 class Bold {
 	static g = 9.82;
 
@@ -31,10 +77,12 @@ class Bold {
 		this.r = r;
 		this.vinkel = vinkel;
 		this.v0 = v0;
+		this.time = 0;
 	}
 
-	opdater(t) {
-
+	opdater(deltat) {
+		this.time += deltat;
+		let t = this.time;
 		let v0x = 0.25 * this.v0 * cos(-this.vinkel);
 		let v0y = 0.5 * this.v0 * sin(-this.vinkel);
 		let led1 = 1 / 2. * (Bold.g) * (t ** 2) * 6;
