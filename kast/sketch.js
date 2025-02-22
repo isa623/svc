cfdae345b128e59f3ac2b4904ba70fa0c828352f
isa623 <NEXT16325@edu.nextkbh.dk>
2025-02-22 11:50:22 +0100

    Bolde lagt ind i array

--- kast/sketch.js
+++ kast/sketch.js
@@ -1,22 +1,27 @@
-let bold1
-let bold2
+let balls = [];
+let angleInputs = [];
+let speedInputs = [];
+
 
 function setup() {
 	createCanvas(800, 800);
 	angleMode(DEGREES);
-	bold1 = new Bold(50, 45, 500);
-	bold2 = new Bold(50, 70, 500);
+	for (let i = 0; i < 3; i++) {
+		let angle = 45;
+		let speed = 500;
+		balls.push(new Bold(50, angle, speed));
+	}
 }
 
 function draw() {
+	t = millis() / 1000;
 	background('blue');
-	let t = millis() / 1000;
-	bold1.opdater(t);
-	bold2.opdater(t);
-	bold1.show();
-	bold2.show();
-}
+	for (let ball of balls) {
+		ball.opdater(t);
+		ball.show();
 
+	}
+}
 class Bold {
 	static g = 9.82;
 
@@ -29,6 +34,7 @@ class Bold {
 	}
 
 	opdater(t) {
+
 		let v0x = 0.25 * this.v0 * cos(-this.vinkel);
 		let v0y = 0.5 * this.v0 * sin(-this.vinkel);
 		let led1 = 1 / 2. * (Bold.g) * (t ** 2) * 6;
@@ -40,7 +46,7 @@ class Bold {
 	show() {
 		this.x = constrain(this.x, 0, width - this.r);
 		this.y = constrain(this.y, -999, height - this.r);
-		circle(this.x, this.y, this.r * 2);
 		fill('red');
+		circle(this.x, this.y, this.r * 2);
 	}
 }
